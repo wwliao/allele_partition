0f7850b3669020fd372b8aaf41610792059e8c5b
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2022-05-08 23:47:06 -0400

    Enable to process biallelic records

--- partition_alleles.py
+++ partition_alleles.py
@@ -14,35 +14,44 @@ outvcf = Writer(f"{prefix}.partitioned.vcf.gz", invcf)
 for record in invcf:
     clusters = []
     num_alleles = len(record.ALT)
-    for count1, allele1 in sorted(zip(record.INFO.get("AC"), record.ALT), reverse=True):
-        is_added = False
-        # Why there are some alleles doesn't have any counts?
-        if count1 > 0:
-            for i, cluster in enumerate(clusters):
-                for count2, allele2 in cluster:
-                    if abs(len(allele2) - len(allele1)) < 50:
-                        clusters[i].append((count1, allele1))
-                        is_added = True
-                        break
-            if not is_added:
-                clusters.append([(count1, allele1)])
-   
-    alleles = []
-    counts = []
-    an = record.INFO.get("AN")
-    ns = record.INFO.get("NS")
-    freqs = []
-    for cluster in clusters:
-        allele = cluster[0][1]
-        count = sum(t[0] for t in cluster)
-        alleles.append(allele)
-        counts.append(count)
-        freqs.append(f"{count/an:.6f}")
-    alt = ",".join(alleles)
-    ac = ",".join(map(str, counts))
-    af = ",".join(freqs)
-    record_str = f"{record.CHROM}\t{record.POS}\t{record.ID}\t{record.REF}\t{alt}\t60\t.\tAC={ac};AF={af};AN={an};NS={ns}"
-    out_record = outvcf.variant_from_string(record_str)
-    outvcf.write_record(out_record)
+    if num_alleles > 1:
+        for count1, allele1 in sorted(zip(record.INFO.get("AC"), record.ALT), reverse=True):
+            is_added = False
+            # Why there are some alleles doesn't have any counts?
+            if count1 > 0:
+                for i, cluster in enumerate(clusters):
+                    for count2, allele2 in cluster:
+                        if abs(len(allele2) - len(allele1)) < 50:
+                            clusters[i].append((count1, allele1))
+                            is_added = True
+                            break
+                if not is_added:
+                    clusters.append([(count1, allele1)])
+       
+        alleles = []
+        counts = []
+        an = record.INFO.get("AN")
+        ns = record.INFO.get("NS")
+        freqs = []
+        for cluster in clusters:
+            allele = cluster[0][1]
+            count = sum(t[0] for t in cluster)
+            alleles.append(allele)
+            counts.append(count)
+            freqs.append(f"{count/an:.6f}")
+        alt = ",".join(alleles)
+        ac = ",".join(map(str, counts))
+        af = ",".join(freqs)
+        record_str = f"{record.CHROM}\t{record.POS}\t{record.ID}\t{record.REF}\t{alt}\t60\t.\tAC={ac};AF={af};AN={an};NS={ns}"
+        out_record = outvcf.variant_from_string(record_str)
+        outvcf.write_record(out_record)
+    else:
+        ac = record.INFO.get("AC")
+        af = f"{record.INFO.get('AF'):.6f}"
+        an = record.INFO.get("AN")
+        ns = record.INFO.get("NS")
+        record_str = f"{record.CHROM}\t{record.POS}\t{record.ID}\t{record.REF}\t{record.ALT[0]}\t60\t.\tAC={ac};AF={af};AN={an};NS={ns}"
+        out_record = outvcf.variant_from_string(record_str)
+        outvcf.write_record(out_record)
 invcf.close()
 outvcf.close()

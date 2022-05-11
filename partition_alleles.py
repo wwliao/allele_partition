335726528c245b4537b571a6d2eaae7e304d0055
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2022-05-10 23:45:51 -0400

    Consider REF allele as well; Output grouping results in INFO fields

--- partition_alleles.py
+++ partition_alleles.py
@@ -9,49 +9,48 @@ parser.add_argument("vcffile")
 args = parser.parse_args()
 
 invcf = VCF(args.vcffile)
+invcf.add_info_to_header({"ID": "CLUSTER_AC", "Number": ".", "Type": "Integer", "Description": "Total number of alleles in called genotypes after clustering"})
+invcf.add_info_to_header({"ID": "CLUSTER_AF", "Number": ".", "Type": "Float", "Description": "Estimated allele frequency in the range (0,1] after clustering"})
+invcf.add_info_to_header({"ID": "CLUSTER_AI", "Number": ".", "Type": "String", "Description": "Representative allele index after clustering"})
 prefix = re.search("(\S+)\.vcf\.gz", basename(args.vcffile))[1]
 outvcf = Writer(f"{prefix}.partitioned.vcf.gz", invcf)
 for record in invcf:
     clusters = []
-    num_alleles = len(record.ALT)
-    if num_alleles > 1:
-        for count1, allele1 in sorted(zip(record.INFO.get("AC"), record.ALT), reverse=True):
-            is_added = False
-            # Why there are some alleles doesn't have any counts?
-            if count1 > 0:
-                for i, cluster in enumerate(clusters):
-                    for count2, allele2 in cluster:
-                        if abs(len(allele2) - len(allele1)) < 50:
-                            clusters[i].append((count1, allele1))
-                            is_added = True
-                            break
-                if not is_added:
-                    clusters.append([(count1, allele1)])
-       
-        alleles = []
-        counts = []
-        an = record.INFO.get("AN")
-        ns = record.INFO.get("NS")
-        freqs = []
-        for cluster in clusters:
-            allele = cluster[0][1]
-            count = sum(t[0] for t in cluster)
-            alleles.append(allele)
-            counts.append(count)
-            freqs.append(f"{count/an:.6f}")
-        alt = ",".join(alleles)
-        ac = ",".join(map(str, counts))
-        af = ",".join(freqs)
-        record_str = f"{record.CHROM}\t{record.POS}\t{record.ID}\t{record.REF}\t{alt}\t60\t.\tAC={ac};AF={af};AN={an};NS={ns}"
-        out_record = outvcf.variant_from_string(record_str)
-        outvcf.write_record(out_record)
+    num_alt = len(record.ALT)
+    if num_alt == 1:
+        ref_count = record.INFO.get("AN") - record.INFO.get("AC")
+        allele_list = sorted(zip([ref_count, record.INFO.get("AC")], range(num_alt + 1), [record.REF] + record.ALT), reverse=True)
     else:
-        ac = record.INFO.get("AC")
-        af = f"{record.INFO.get('AF'):.6f}"
-        an = record.INFO.get("AN")
-        ns = record.INFO.get("NS")
-        record_str = f"{record.CHROM}\t{record.POS}\t{record.ID}\t{record.REF}\t{record.ALT[0]}\t60\t.\tAC={ac};AF={af};AN={an};NS={ns}"
-        out_record = outvcf.variant_from_string(record_str)
-        outvcf.write_record(out_record)
+        ref_count = record.INFO.get("AN") - sum(record.INFO.get("AC"))
+        allele_list = sorted(zip([ref_count] + list(record.INFO.get("AC")), range(num_alt + 1), [record.REF] + record.ALT), reverse=True)
+    for count1, idx1, allele1 in allele_list:
+        is_added = False
+        # Why there are some alleles doesn't have any counts?
+        #if count1 > 0:
+        for i in range(len(clusters)):
+            for count2, idx2, allele2 in clusters[i]:
+                if abs(len(allele2) - len(allele1)) < 50:
+                    clusters[i].append((count1, idx1, allele1))
+                    is_added = True
+                    break
+            if is_added:
+                break
+        if not is_added:
+            clusters.append([(count1, idx1, allele1)])
+
+    alleles = []
+    counts = []
+    freqs = []
+    an = record.INFO.get("AN")
+    for cluster in clusters:
+        alleles.append("-".join([str(t[1]) for t in cluster]))
+        count = sum(t[0] for t in cluster)
+        counts.append(count)
+        freqs.append(f"{count/an:.6f}")
+
+    record.INFO["CLUSTER_AI"] = ",".join(alleles)
+    record.INFO["CLUSTER_AC"] = ",".join(map(str, counts))
+    record.INFO["CLUSTER_AF"] = ",".join(freqs)
+    outvcf.write_record(record)
 invcf.close()
 outvcf.close()
